2d48d06c344603c9ed04d04542382ad61b2cecd5
lyman wong <lymanwong@gmail.com>
2015-03-19 16:59:23 -0700

    added ability for commenter to add images to their comments

--- db/schema.rb
+++ db/schema.rb
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 20150318192258) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
-    t.string   "string"
     t.text     "body"
+    t.text     "commenterimage"
     t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
