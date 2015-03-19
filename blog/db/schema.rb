033b7aa7fe7b809528fac9f624bfaed84334994f
lyman wong <lymanwong@gmail.com>
2015-03-18 17:21:39 -0700

    fixing

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 20150318192258) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
-    t.string   "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
