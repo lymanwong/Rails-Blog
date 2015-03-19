b6e8ebaa75a78e1df5f0639118bc7779184f4d20
lyman wong <lymanwong@gmail.com>
2015-03-18 18:18:49 -0700

    fixing

--- blog/db/migrate/20150318171844_create_articles.rb
+++ blog/db/migrate/20150318171844_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
       t.text :text
-
+      t.text :image
       t.timestamps null: false
     end
   end
